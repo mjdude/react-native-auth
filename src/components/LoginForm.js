354595fd77f57c4efca992e7e2ef80fd42ff393f
mjdude <mo@syncspot.net>
2017-04-08 18:27:45 -0700

    added password text entry

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,18 +2,31 @@ import React, {Component}  from 'react';
 import {Button, Card, CardSection, Input} from './common'
 
 class LoginForm extends Component {
-    state = {text:''}
+    state = {
+        email:'',
+        password: ''
+    }
     render(){
         return (
         <Card>
             <CardSection>
                 <Input 
+                placeholder="user@example.com"
                 label="Email"
-                value={this.state.text}
-                onChangeText={text => this.setState({text})}>
+                value={this.state.email}
+                onChangeText={email => this.setState({email})}>
                 </Input>
             </CardSection>
-            <CardSection></CardSection>
+            <CardSection>
+                <Input
+                secureTextEntry={true}
+                placeholder="password"
+                label="Password"
+                value={this.state.password}
+                onChangeText={ password => this.setState({password})}
+                >
+                </Input>
+            </CardSection>
             <CardSection>
                 <Button>
                     Login
