a3de75169fa55552ad8ee10051582ef7bcfdb788
mjdude <mo@syncspot.net>
2017-04-08 20:55:48 -0700

    added spinner

---
+++ src/components/common/Spinner.js
@@ -0,0 +1,12 @@
+import React, {Component}  from 'react';
+import {TextInput, View, Text} from 'react-native';
+
+const Spinner = () => {
+    return (
+        <View >
+            <Text></Text>
+        </View>
+    )
+}
+
+export { Spinner }
--- src/components/common/index.js
+++ src/components/common/index.js
@@ -4,5 +4,6 @@ export * from './Card';
 export * from './CardSection';
 export * from './Header';
 export * from './Input';
+export * from './Spinner';
 
 
